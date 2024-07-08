df0cb4474a33284b343ff575d041c19e47815129
Frank Kenneth Spacey Helder <franksh14@gmail.com>
2024-07-08 14:21:09 +0000

    new save2

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,11 +11,11 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-light bg-light mb-3">
-      <Link to="/">
+      <Link className="swBtn" to="/">
         <span className="navbar-brand mb-0 h1">Starwars Blog</span>
       </Link>
       <div className="ml-auto">
-        <div className="btn-group">
+        <div className="btn-group dropstart">
           <button
             type="button"
             className="btn btn-primary dropdown-toggle"
--- src/js/views/CharacterList.js
+++ src/js/views/CharacterList.js
@@ -1,27 +1,29 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { Context } from "../store/appContext";
 import { CharacterCard } from "../component/CharacterCard";
 
 export const CharacterList = () => {
   const { store, actions } = useContext(Context);
+  const carouselRef = useRef(null);
 
+  // Scroll to beginning of carousel on component load
   useEffect(() => {
-    actions.getPeople("people");
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft = 0;
+    }
   }, []);
-
-  useEffect(() => {
-    console.log("Store listCharacters:", store.listCharacters); // Debugging line
-  }, [store.listCharacters]);
 
   return (
     <div className="container">
       <h1>Characters</h1>
       <div className="row">
-        {store.listCharacters.map((character, index) => (
-          <div key={index} className="col-md-4">
-            <CharacterCard character={character} />
-          </div>
-        ))}
+        <div ref={carouselRef} className="card-carousel">
+          {store.listCharacters.map((character, index) => (
+            <div key={index} className="card-carousel-item">
+              <CharacterCard character={character} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
